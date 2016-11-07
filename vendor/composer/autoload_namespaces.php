b221b42ae6670f4122d4a927797360ca551e76cf
Anwar <anwar@bglobalsourcing.com>
2016-11-07 20:37:32 +0600

    doctrine added

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -11,7 +11,10 @@ return array(
     'JakubOnderka\\PhpConsoleHighlighter' => array($vendorDir . '/jakub-onderka/php-console-highlighter/src'),
     'JakubOnderka\\PhpConsoleColor' => array($vendorDir . '/jakub-onderka/php-console-color/src'),
     'Gregwar\\Captcha' => array($vendorDir . '/gregwar/captcha'),
+    'Doctrine\\DBAL\\' => array($vendorDir . '/doctrine/dbal/lib'),
+    'Doctrine\\Common\\Lexer\\' => array($vendorDir . '/doctrine/lexer/lib'),
     'Doctrine\\Common\\Inflector\\' => array($vendorDir . '/doctrine/inflector/lib'),
+    'Doctrine\\Common\\Collections\\' => array($vendorDir . '/doctrine/collections/lib'),
     'Cron' => array($vendorDir . '/mtdowling/cron-expression/src'),
     'Cartalyst\\Sentry' => array($vendorDir . '/jacopo/authentication-sentry/src'),
 );
